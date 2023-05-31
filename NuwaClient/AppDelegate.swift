0e74bdb682ddb2d9d1c9ba5d6093eac9d6ba4a7c
ConradSun <sunkangnuaa@163.com>
2023-05-31 17:01:08 +0800

    Add the status bar menu for ease of use

--- NuwaClient/AppDelegate.swift
+++ NuwaClient/AppDelegate.swift
@@ -13,8 +13,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var stopMenuItem: NSMenuItem!
     @IBOutlet weak var clearMenuItem: NSMenuItem!
     
+    let menuBar = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         setMenuStatus(start: true, stop: false)
+        setupMenuBar()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -30,17 +33,47 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             return true
         }
         
-        for window: AnyObject in sender.windows {
-            if window.frameAutosaveName == "NuwaStoneMainWindow" {
-                window.makeKeyAndOrderFront(self)
-                return true
-            }
+        if let window = getMainWindow(sender) {
+            window.makeKeyAndOrderFront(self)
         }
         return true
+    }
+}
+
+extension AppDelegate {
+    @objc func hideDockIcon() {
+        NSApp.setActivationPolicy(.accessory)
+        getMainWindow(NSApplication.shared)?.close()
+    }
+    
+    @objc func showDockIcon() {
+        NSApp.setActivationPolicy(.regular)
+        NSApp.activate(ignoringOtherApps: true)
+        getMainWindow(NSApplication.shared)?.makeKeyAndOrderFront(self)
+    }
+    
+    func getMainWindow(_ sender: NSApplication) -> NSWindow? {
+        for window: AnyObject in sender.windows {
+            if window.frameAutosaveName == "NuwaStoneMainWindow" {
+                return window as? NSWindow
+            }
+        }
+        return nil
     }
     
     func setMenuStatus(start: Bool, stop: Bool) {
         startMenuItem.isEnabled = start
         stopMenuItem.isEnabled = stop
+    }
+    
+    func setupMenuBar() {
+        let statusMenu = NSMenu()
+        statusMenu.addItem(withTitle: "Run in Background", action: #selector(hideDockIcon), keyEquivalent: "")
+        statusMenu.addItem(withTitle: "Show App Window", action: #selector(showDockIcon), keyEquivalent: "")
+        statusMenu.addItem(withTitle: "Quit NuwaStone", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
+        
+        menuBar.button!.image = NSImage(named: NSImage.Name("MenuIcon"))
+        menuBar.button!.toolTip = "NuwaStone"
+        menuBar.menu = statusMenu
     }
 }
